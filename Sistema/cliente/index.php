2b26492e674c096933f79d49e9af6fcd8aa1cc04
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-01-14 18:51:14 -0500

    Consulta Cliente creada
    De manera análoga a la página producto, se hizo con cliente.
    
    Ya funcional.

--- Sistema/cliente/index.php
+++ Sistema/cliente/index.php
@@ -9,5 +9,75 @@
 	<h1>Cliente</h1>
 	<br>
 	<a href="..\">Retornar</a>
+
+	<hr>
+
+	<!-- CODIGO PHP -->
+	<?php
+		// Inicializando otras variables:
+		$tabla = array();
+
+		// Si existe el valor enviado:
+		if (isset($_GET["txtValor"])) {
+			// Se recoje y almacena:
+			$valor = $_GET["txtValor"];
+
+			// Solo si la cadena es un poco laga, lo buscará:
+			if (strlen($valor)>=2) {
+				// Incluir un archivo PHP:
+				require_once "../config/conexion.php";
+
+				// Preparar la sentencia SQL:
+				//$sentencia = $cnx->prepare("select * from producto");
+				$sentencia = $cnx->prepare("SELECT * FROM cliente WHERE nombre LIKE CONCAT('%', :nombre, '%');");
+
+				// Pasamos el parámetr SQL:
+				$sentencia->bindvalue(":nombre", $valor);
+
+				// Ejecutamos dicha sentencia:
+				$sentencia->execute();
+
+				// Recojemos las filas generadas en una matriz bidimensional:
+				$tabla = $sentencia->fetchAll();
+			}
+		} else {
+			// Sino, este será el valor predeterminado:
+			$valor = "";
+		}
+	?>
+
+	<form method="get">
+		<label>Nombre del cliente a buscar:</label><br>
+		<input type="text" name="txtValor" value="<?php echo $valor ?>">
+		<input type="submit" name="btnBuscar" value="Buscar">
+	</form>
+	<br>
+
+	<table border=1>
+		<tr>
+			<th>Código</th>
+			<th>Nombre</th>
+			<th>RUC</th>
+			<th>Dirección</th>
+			<th>Teléfono</th>
+		</tr>
+
+	<?php
+		foreach ($tabla as $registro) {
+	?>
+
+		<tr>
+			<td><?php echo $registro["id"]; ?></td>
+			<td><?php echo $registro["nombre"]; ?></td>
+			<td><?php echo $registro["numruc"]; ?></td>
+			<td><?php echo $registro["direccion"]; ?></td>
+			<td><?php echo $registro["telefono"]; ?></td>
+		</tr>
+
+	<?php
+		}
+	?>
+
+	</table>
 </body>
 </html>
